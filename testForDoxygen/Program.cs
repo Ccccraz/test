6194225adfa248a229cf1b0950f97a930e9f36c3
Ccccraz <hy.ccccr@gmail.com>
2024-01-30 17:31:45 +0800

    first commit

---
+++ testForDoxygen/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace testForDoxygen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ testForDoxygen/TestForDoxygen.cs
@@ -0,0 +1,19 @@
+﻿namespace testForDoxygen;
+
+public class TestForDoxygen
+{
+    /// <summary>
+    /// 比较两个数字的大小
+    /// </summary>
+    /// <param name="a"> 第一个数字 test test </param>
+    /// <param name="b"> 第二个数字 </param>
+    /// <returns>
+    /// 如果第一个数字大于第二个数字则返回 true 否则返回 false
+    /// Return true if the first number is greater than the second number otherwise false
+    /// </returns>
+    public bool Size(int a, int b)
+    {
+        return a > b;
+    }
+    
+}
